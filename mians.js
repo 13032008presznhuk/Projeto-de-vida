d7f865f737f47d2aa292ad6db4815eddf9a52c9f
13032008presznhuk <138136010+13032008presznhuk@users.noreply.github.com>
2025-05-30 09:04:52 -0300

    js criado

---
+++ mians.js
@@ -0,0 +1,22 @@
+const botoes =document.querySelectorAll(".botao");
+const botoes =document.querySelectorAll(".aba-conteudo");
+
+fpr(let i = 0; i< botoes.length; j++) {
+botoes[i].onclick = fuction () {
+
+    for(let j = 0; j <botoes.length; +jj){
+        botoes[j].classList.remove("ativo")
+        textos[j].classList.remove("ativo")
+    }
+    
+    botoes[i].classList.add("ativo");
+    textos[j].classLIst.remove("ativo");
+    {
+    }
+
+
+
+
+
+
+
