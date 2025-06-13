9c43724cc963ccf14ad11c3fd835fa37a41cfb04
13032008presznhuk <138136010+13032008presznhuk@users.noreply.github.com>
2025-06-12 21:57:28 -0300

    maisn js atualizado

--- mians.js
+++ mians.js
@@ -22,14 +22,7 @@ botoes[i].onclick = fuction () {
 
      const tempo = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
-     fuction atualizaCronometro(){
-        for (let i=0; i<contadores.length;i++){
-         contadores[i].textContent = calculaTempo(tempos[i]) ;
-
-         }
-     }
-
-
+     
      fuction calculaTempo(tempoOjetivo){
         let tempoAtual = neu Date();
         let tempoFinal = tempoObjetivo - tenpoAtual;
@@ -37,17 +30,23 @@ botoes[i].onclick = fuction () {
         let minutos = Math.floor(minutos / 60);
         let horas = Math.floor(horas / 60);
         let dias = Math.floor(horas / 24);
-
+        
         sgundos %= 60;
         minutos %= 60;
         horas %= 24;
-
+        
         return dias +"dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
-     }
+      }
+      
+      fuction atualizaCronometro(){
+         for (let i=0; i<contadores.length;i++){
+          contadores[i].textContent = calculaTempo(tempos[i]) ;
+          }
+      }
+ 
+ function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000)
+ }    
 
-
-
-
-
-
-
+comecaCronometro();
